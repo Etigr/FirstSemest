83eecb56d3ba1b042b9796d0cf64f50d5ce66b74
Evgen <fadi_86@mail.ru>
2022-12-07 02:02:48 +0300

    Создание метода вывода печати введёного массива создание нового массива из 3 символов

--- Program.cs
+++ Program.cs
@@ -19,3 +19,17 @@ void PrintUserArray(string[] ArrayUser)
     Console.Write($" {ArrayUser[i],1}, "); 
     Console.Write("\b\b] \n");
 }
+void ArrayThreeElenent(string[] ArrayUser)
+{
+  string[] NewArray = new string[ArrayUser.Length];
+// Console.WriteLine($"");
+Console.WriteLine($"Выборка элементов массива не более 3 символов:");
+Console.Write($"[");
+  for (int i = 0; i < ArrayUser.Length; i++)
+    if (ArrayUser[i].Length < 4)
+    {
+      NewArray[i] = ArrayUser[i];
+      Console.Write($" {NewArray[i],1}, ");
+    }
+    Console.Write("\b\b] \n");
+}
