226581a614558dd4a53ad8c67d5999ec4fc1a235
Evgen <fadi_86@mail.ru>
2022-12-07 01:59:34 +0300

    Создание метода ввода массива пользователем

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] UserArray()
+{
+  Console.Write($"Введи количество элементов массива: ");
+  int ElemenArr = Convert.ToInt32(Console.ReadLine());
+  string[] ArrayUser = new string[ElemenArr];
+
+    for (int i = 0; i < ArrayUser.Length; i++)
+    {
+    Console.Write($"Введи значение {i}-го элемента массива: ");
+    ArrayUser[i] = Console.ReadLine();
+    }
+  return ArrayUser;
+}
